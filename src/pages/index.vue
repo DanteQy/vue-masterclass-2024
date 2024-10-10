fbd534bf09d8b4c638864f5fd4113f3fa3441ca0
Dante Qyshka <dqyshka@ges.com>
2024-10-10 16:17:06 +0200

    added tasks component

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -4,5 +4,9 @@
   <div>
     <h1>Home Page</h1>
     <RouterLink :to="{ name: '/projects/[id]', params: { id: 1 } }">Go to Project 1</RouterLink>
+    <br />
+    <RouterLink :to="'/projects'">Go to Projects</RouterLink>
+    <br />
+    <RouterLink :to="'/tasks'">Go to Tasks</RouterLink>
   </div>
 </template>
