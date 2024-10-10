4bf2e5c41c7695ec740913459b51113e0099eb1c
Dante Qyshka <dqyshka@ges.com>
2024-10-10 16:24:08 +0200

    added shacdn and tailwindcss

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,19 @@ import VueRouter from 'unplugin-vue-router/vite'
 
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-
+import tailwind from 'tailwindcss'
+import autoprefixer from 'autoprefixer'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [VueRouter(), vue()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  css: {
+    postcss: {
+      plugins: [tailwind(), autoprefixer()]
     }
   }
 })
