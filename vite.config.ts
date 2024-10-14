b40447f127b8384c07d8cea0c8aee05548c43942
Dante Qyshka <dqyshka@ges.com>
2024-10-14 16:41:43 +0200

    configured auto import of components

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,9 @@
 import { fileURLToPath, URL } from 'node:url'
 import VueRouter from 'unplugin-vue-router/vite'
 import AutoImport from 'unplugin-auto-import/vite'
+import Components from 'unplugin-vue-components/vite'
+import { VueRouterAutoImports } from 'unplugin-vue-router'
+
 import tailwind from 'tailwindcss'
 import autoprefixer from 'autoprefixer'
 
@@ -11,6 +14,9 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [
     VueRouter(),
+    Components({
+      /* options */
+    }),
     AutoImport({
       include: [
         /\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
@@ -18,7 +24,7 @@ export default defineConfig({
         /\.vue\?vue/, // .vue
         /\.md$/ // .md
       ],
-      imports: ['vue', 'vue-router'],
+      imports: ['vue', VueRouterAutoImports],
       dts: true,
       viteOptimizeDeps: true
     }),
