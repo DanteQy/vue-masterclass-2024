6b0bdb12b2ee2a445d1f95b9911396e140feaff4
Dante Qyshka <dqyshka@ges.com>
2024-10-14 17:15:39 +0200

    added async management in app component

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,15 @@ import AuthLayout from '@/components/Layout/main/AuthLayout.vue'
 <template>
   <auth-layout>
     <main>
-      <RouterView />
+      <RouterView v-slot="{ Component, route }">
+        <Suspense v-if="Component" #fallback>
+          <Component :is="Component" :key="route.name"></Component>
+
+          <template #fallback>
+            <span>Loading ..</span>
+          </template>
+        </Suspense>
+      </RouterView>
     </main>
   </auth-layout>
 </template>
--- src/pages/projects/index.vue
+++ src/pages/projects/index.vue
@@ -4,7 +4,7 @@ import type { Tables } from '../../../database/types'
 import type { ColumnDef } from '@tanstack/vue-table'
 
 const projects = ref<Tables<'projects'>[] | null>(null)
-;(async () => {
+const getProjects = async () => {
   const { data, error } = await supabase.from('projects').select()
 
   if (error) console.log(error)
@@ -12,7 +12,9 @@ const projects = ref<Tables<'projects'>[] | null>(null)
   projects.value = data
 
   console.log('projects: ', projects.value)
-})()
+}
+
+await getProjects()
 
 const columns: ColumnDef<Tables<'projects'>>[] = [
   {
