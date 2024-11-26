5f2eb3a6eaa29f6300e41f1069c6b829fa73dda5
Dante Qyshka <dqyshka@ges.com>
2024-11-26 10:31:20 +0100

    added component to edit text from detail view
    - created component to allow inline edit
    - modified slug component to allow edit of content when focused

---
+++ src/components/AppInPlaceEdit/AppInPlaceEditText.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+const value = defineModel()
+</script>
+
+<template>
+  <input
+    class="w-full p-1 bg-transparent focus-visible:outline-none focus:border-none focus:bg-gray-800 focus:rounded-md"
+    type="text"
+    v-model="value"></input>
+</template>
--- src/pages/projects/[slug].vue
+++ src/pages/projects/[slug].vue
@@ -19,7 +19,9 @@ await getProject(slug)
   <Table v-if="project">
     <TableRow>
       <TableHead> Name</TableHead>
-      <TableCell> {{ project.name }}</TableCell>
+      <TableCell>
+        <app-in-place-edit-text v-model="project.name" />
+      </TableCell>
     </TableRow>
     <TableRow>
       <TableHead> Description</TableHead>
